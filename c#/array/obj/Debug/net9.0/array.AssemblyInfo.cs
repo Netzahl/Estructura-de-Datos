bd5deb206e355bc20872548f9e01c6b60a5f1548
Netzahl <mariodavidln@gmail.com>
2025-09-02 09:47:05 -0700

    Detalles menorres

--- c#/array/obj/Debug/net9.0/array.AssemblyInfo.cs
+++ c#/array/obj/Debug/net9.0/array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736f8b7d50f9a16dfce6c8d7fcc4d2f0a783bff5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7700daf066fac0886c9d340b45f968ce9bf6b763")]
 [assembly: System.Reflection.AssemblyProductAttribute("array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
