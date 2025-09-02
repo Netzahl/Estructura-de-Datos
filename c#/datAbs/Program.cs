3292150f3b4eb6e6e97ca66706faec31c5de9275
Netzahl <mariodavidln@gmail.com>
2025-09-02 09:59:13 -0700

    Crea un array de datos abstractos en c#

---
+++ c#/datAbs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+class Persona
+{
+    public string nombre;
+    public int edad;
+
+    public Persona()
+    {
+        nombre = "";
+        edad = 0;
+    }
+
+    public void mostrarDatos()
+    {
+        Console.WriteLine($"Nombre: {nombre}, Edad:{edad}");
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Persona[] personas = new Persona[3];
+        personas[0] = new Persona();
+        personas[0].nombre = "David";
+        personas[0].edad = 19;
+        personas[0].mostrarDatos();
+    }
+}
--- c#/datAbs/obj/Debug/net9.0/datAbs.AssemblyInfo.cs
+++ c#/datAbs/obj/Debug/net9.0/datAbs.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("array")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("datAbs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5deb206e355bc20872548f9e01c6b60a5f1548")]
-[assembly: System.Reflection.AssemblyProductAttribute("array")]
-[assembly: System.Reflection.AssemblyTitleAttribute("array")]
+[assembly: System.Reflection.AssemblyProductAttribute("datAbs")]
+[assembly: System.Reflection.AssemblyTitleAttribute("datAbs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
