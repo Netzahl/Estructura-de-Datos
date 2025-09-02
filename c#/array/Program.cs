7700daf066fac0886c9d340b45f968ce9bf6b763
Netzahl <mariodavidln@gmail.com>
2025-09-02 09:44:36 -0700

    Crea un array simple en C#

---
+++ c#/array/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[3];
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+
+        Console.WriteLine(numeros[0]);
+
+    }
+}
