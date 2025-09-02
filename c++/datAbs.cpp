babfd7fcfac0d21f6d78bfd12849fd9150073a68
Netzahl <mariodavidln@gmail.com>
2025-09-02 09:20:03 -0700

    Crea un arreglo con datos abstractos

---
+++ c++/datAbs.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Persona{
+public:
+    string nombre;
+    int edad;
+
+    Persona(){
+
+        nombre = "Desconocido";
+        edad = 0;
+
+    }
+
+    void mostrarDatos(){
+
+        cout<<"Nombre "<<nombre<<", Edad: "<<edad<<endl;
+
+    }
+
+};
+
+int main(){
+
+    Persona wey[3];
+
+    wey[0].nombre = "David";
+    wey[0].edad = 19;
+
+    wey[0].mostrarDatos();
+
+    return 0;
+
+}
