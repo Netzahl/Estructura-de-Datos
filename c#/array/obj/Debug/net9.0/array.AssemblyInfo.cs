58211cfa5f7c88e7cb9552805b55b312fe6868da
Netzahl <mariodavidln@gmail.com>
2025-09-08 01:11:58 -0700

    Crea un archivo de busqueda lineal
    En java crea un archivo que contenga un array donde se ejecute una busqueda lineal, con  numeros aleatorios como datos.

--- c#/array/obj/Debug/net9.0/array.AssemblyInfo.cs
+++ c#/array/obj/Debug/net9.0/array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89815dc737255ea828b0ba5335e01fdc31d014a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993c424147ddc6da6843d7939aa2b232d3e9979e")]
 [assembly: System.Reflection.AssemblyProductAttribute("array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
