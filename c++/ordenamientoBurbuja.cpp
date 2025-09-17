93edc9f6078ea2c7a62f85480a214b19199709c3
Netzahl <mariodavidln@gmail.com>
2025-09-16 17:20:19 -0700

    Create ordenamientoBurbuja.cpp

---
+++ c++/ordenamientoBurbuja.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main(){
+    random_device rd;
+    default_random_engine gen(rd());
+    uniform_int_distribution<int> dist(1,20);
+
+    int numeros[10];
+    int adicional = 0;
+
+    for(int i=0;i<10;i++){
+        numeros[i] = dist(gen);
+    }   
+
+    cout << " | ";
+    for(int i=0;i<10;i++){
+        cout << numeros[i] << " | ";
+    }   
+    cout << endl;
+
+    for(int j = 9;j>=0;j--){
+        for(int i=1;i<j+1;i++){
+            if (numeros[i-1] > numeros[i]){
+                adicional = numeros[i-1];
+                numeros[i-1] = numeros[i];
+                numeros[i] = adicional;
+            }
+        }   
+    }
+    
+    cout << " | ";
+    for(int i=0;i<10;i++){
+        cout << numeros[i] << " | ";
+    }   
+
+
+}
